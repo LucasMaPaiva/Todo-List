2dd4cffe11af5e7d6c159829308d2a8fdb0c3519
Paiva <matheuspaiva935@gmail.com>
2024-04-07 04:34:28 -0400

    First commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,11 +5,31 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
-                    <div class="card-header text-center">
-                        Todo List
+                    <div class="card-header d-flex justify-content-between mb-0">
+                        <div class="card-title mt-2 mb-0">
+                            Todo List
+                        </div>
+                        <div>
+                            <div class="btn btn-primary nav-link">
+                                Add Task
+                            </div>
+                        </div>
                     </div>
                     <div class="card-body">
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th scope="col">Task</th>
+                                    <th scope="col">Actions</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach($tasks as $task)
 
+
+                                @endforeach
+                            </tbody>
+                        </table>
                     </div>
                 </div>
             </div>
